b7bf9aafdd7ca265178a10ea9dc2142b4a09d6bf
chienthinkpad <chienptit@gmail.com>
2019-09-20 16:45:22 +0700

    ptb2

--- if-else.c
+++ if-else.c
@@ -1,13 +1,15 @@
 #include<stdio.h>
+#include<math.h>
+#include<conio.h>
 int main(){
 	int a;
-	scanf("%d",&a);
-	printf("nhap vao so tien=%d",a);
+	scanf("%d", &a);
+	printf("So tien cua ban la %d", a);
 	if(a>=1000){
-		printf("\nA la trieu phu");
+		printf("\n=>Ban la trieu phu");
 	}
 	else{
-		printf("\nA khong la trieu phu");
+		printf("\n=>Ban chua la trieu phu");
 	}
 	return 0;
 }
