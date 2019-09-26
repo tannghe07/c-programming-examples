387e72855037fc669fe3d742fb8aa1229123081b
chienthinkpad <chienptit@gmail.com>
2019-09-26 16:41:12 +0700

    Triangle Pascal.c

--- Untitled11.c
+++ Untitled11.c
@@ -2,9 +2,9 @@
 int main(){
 	int a;
 	scanf("%d", &a);
-	if(a/2==0){
+	if(a%2==0){
 		printf("%d la so chan", a);
-	}else if(a/2!=0){
+	}else if(a%2!=0){
 		printf("%d la so le", a);
 	}
 	return 0;
