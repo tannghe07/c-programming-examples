aa9a18860af22ba0729ff06bafe798ccbae0ec0a
chienthinkpad <tannghe07@gmail.com>
2019-09-29 22:23:34 +0700

    max.c

---
+++ max.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+	int a, b, c, d;
+	scanf("%d%d%d%d", &a, &b, &c, &d );
+	int max = a;
+	if(max<b)max=b;
+	if(max<c)max=c;
+	if(max<d)max=d;
+	if(a==b&&a==c&&a==d)printf("Khong co gia tri lon nhat!");
+	else printf("max=%d", max);
+	return 0;
+}
+
